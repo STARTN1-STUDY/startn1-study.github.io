0592b8b359aa56a2327c7f05704bc43331803459
STARTN1-STUDY <rafaelrdmedicine@gmail.com>
2026-09-16 10:52:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* N1 19-09 -- service worker. Rede primeiro, cache como rede de seguranca.
    Cache-first serviria versao velha depois de um conserto; com prova em 11 dias
    isso e pior que uma ida a rede. */
-var VERSAO = 'n1-1dcf46e34d9d';
+var VERSAO = 'n1-f19bb718f05a';
 var ESSENCIAIS = ['./', './index.html'].concat(["./imagens__anestesia-geral__mallampati.svg","./imagens__esofago-cirurgico__esofago-tercos-anatomia.svg"]);
 var PRAZO = 3500;
 self.addEventListener('install', function(e){
